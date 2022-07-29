211a67249cf955ada01d84e14562a25716c53f6f
aacjlokl <2043339258@qq.com>
2022-07-29 15:23:34 +0800

    v2

--- src/aacjlokl/Test1.java
+++ src/aacjlokl/Test1.java
@@ -3,5 +3,6 @@ package aacjlokl;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello java  v1");
+        System.out.println("hello java  v2");
     }
 }
